f1ccbef457fff31911ca40086ce04d24a3264033
Stephanie Otieno <stephanie.otieno@smart.local>
2024-04-16 06:52:10 -0700

    added the taskbar

--- src/app/taskbar/taskbar.component.ts
+++ src/app/taskbar/taskbar.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
-
+import {
+ 
+  CdkMenu,
+  CdkMenuTrigger,
+  CdkMenuItem,
+  CdkMenuBar,
+} from '@angular/cdk/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
 @Component({
   selector: 'app-taskbar',
   standalone: true,
-  imports: [],
+  imports: [CdkMenu,
+    CdkMenuTrigger,
+    CdkMenuItem,
+    CdkMenuBar,MatMenuModule,MatToolbarModule],
   templateUrl: './taskbar.component.html',
   styleUrl: './taskbar.component.css'
 })
