7c03d4333f00e515401445655682489ddf079384
Stephanie Otieno <stephanie.otieno@smart.local>
2024-04-18 02:51:09 -0700

    base consisting of the app body

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TaskbarComponent } from "./taskbar/taskbar.component";
 import { BaseComponent } from "./base/base.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports: [CommonModule, RouterOutlet, TaskbarComponent, BaseComponent]
+    imports: [CommonModule, RouterOutlet, TaskbarComponent, BaseComponent,CommonModule],
 })
-export class AppComponent {
-  title = 'nail-appointment';
+export class AppComponent  {
+ 
+  
+  
 }
--- src/app/base/base.component.ts
+++ src/app/base/base.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { GoogleMapsModule } from '@angular/google-maps'
 @Component({
   selector: 'app-base',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,GoogleMapsModule],
   templateUrl: './base.component.html',
   styleUrl: './base.component.css'
 })
 export class BaseComponent {
+
+isImageLoaded:boolean=false;
+ngOnInit(): void {
+  setTimeout(()=>{
+    this.isImageLoaded=true;
+  },2000);
+}
 
 }
